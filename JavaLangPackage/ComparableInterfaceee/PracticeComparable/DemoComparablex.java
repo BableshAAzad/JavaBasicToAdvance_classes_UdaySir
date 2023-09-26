bf78fe8e7bedb55dc0a196ef662318d08e08953c
BableshAAzad <bableshaazad@outlook.com>
2023-09-26 22:59:35 +0530

    practice comparable

---
+++ JavaLangPackage/ComparableInterfaceee/PracticeComparable/DemoComparablex.java
@@ -0,0 +1,10 @@
+package PracticeComparable;
+
+public class DemoComparablex {
+    // Integer.valueOf(x); 
+    public static void main(String[] args) {
+        Integer a = 30;
+        Integer b = 50;
+        System.out.println(a.compareTo(b));
+    }
+}
