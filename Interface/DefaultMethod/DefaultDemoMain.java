b654f0f6df81b52799d736bb823104d4c9b309cc
BableshAAzad <bableshaazad@outlook.com>
2023-10-23 22:30:40 +0530

    default method in interface

--- Interface/DefaultMethod/DefaultDemoMain.java
+++ Interface/DefaultMethod/DefaultDemoMain.java
@@ -1,26 +1,26 @@
-package AbstractMethod;
+package DefaultMethod;
 
-interface AbstractDemo1 {
+interface DefaultDemo1 {
     public default void m1() {
         System.out.println("Abstract method m1()");
     }
 }
 
-interface AbstractDemo2 {
+interface DefaultDemo2 {
     public default void m1() {
         System.out.println("Abstract method m2()");
     }
 }
 
-public class AbstractDemoMain implements AbstractDemo1, AbstractDemo2 {
+public class DefaultDemoMain implements DefaultDemo1, DefaultDemo2 {
     @Override
     public void m1() {
-        AbstractDemo1.super.m1();
+        DefaultDemo1.super.m1();
         System.out.println("Overrided m1()");
     }
 
     public static void main(String[] args) {
-        AbstractDemoMain a1 = new AbstractDemoMain();
+        DefaultDemoMain a1 = new DefaultDemoMain();
         a1.m1();
     }
 }
---
+++ Interface/DefaultMethod/DefaultMethodInClass.java
@@ -0,0 +1,8 @@
+package DefaultMethod;
+
+public class DefaultMethodInClass {
+    // default void m1(){
+    //     System.out.println("This is a default method m1()");
+    // }
+    // ! Default methods are allowed only within interfaces
+}
