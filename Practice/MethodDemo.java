3810be1cadfb31bcdf18b36bff00f09a0f76bc61
BableshAAzad <bableshaazad@outlook.com>
2023-11-05 11:59:17 +0530

    practice java oop

---
+++ Practice/MethodDemo.java
@@ -0,0 +1,14 @@
+public class MethodDemo {
+    int b;
+    void m1() {
+        // System.out.println(b);
+        int a;
+        // a = 10;
+        // System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        MethodDemo m = new MethodDemo();
+        m.m1();
+    }
+}
---
+++ Practice/StringDemo.java
@@ -0,0 +1,8 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Bab");
+        // StringBuilder sb1 = new StringBuilder("lesh");
+        // sb.append(sb1);
+        // System.out.println(sb);
+     }
+}
