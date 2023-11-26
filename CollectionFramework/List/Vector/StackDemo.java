e6dc7a736560b7233f68950e89bca657ac4c5377
BableshAAzad <bableshaazad@outlook.com>
2023-11-26 22:57:22 +0530

    Enumeration Stack java

--- CollectionFramework/List/Vector/StackDemo.java
+++ CollectionFramework/List/Vector/StackDemo.java
@@ -14,5 +14,18 @@ public class StackDemo {
         System.out.println(sv.peek());
         // System.out.println(sv.count());
         // System.out.println(change(1));
+        Enumeration<Integer> e = sv.elements();
+        while (e.hasMoreElements()) {
+            System.out.println(e.nextElement());
+        }
     }
 }
+/**
+ * [10, 20, 30, 40, 50]
+ * 50
+ * 10
+ * 20
+ * 30
+ * 40
+ * 50
+ */
