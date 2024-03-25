620c6a97fff8ebb690e39cb0ff8cd30a6b0f5bea
BableshAAzad <bableshaazad@outlook.com>
2024-03-25 22:59:10 +0530

    DOBdifferenceUsingArrayList

---
+++ Practice/DOBdifferenceUsingArrayList.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.util.*;
+
+class Student {
+    private int studentId;
+    private String studentName;
+    private LocalDateTime dob;
+    Student(int studentId, String studentName, LocalDateTime dob){
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.dob = dob;
+    }
+}
+
+public class DOBdifferenceUsingArrayList {
+   public static void main(String[] args) {
+      ArrayList<Student> list = new ArrayList<Studnet>();
+
+   }
+}
